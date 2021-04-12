4a01da9ab9afee4b6d392573b1835b233f6cc594
Matis <marguello@procesoautonomo.com>
2021-04-11 23:54:18 -0300

    Agrego Glassmorphism Card y Utilizo jsInterop de blazor

---
+++ BlazorWebDemo/wwwroot/js/VanillaTilt/vanilla-tilt-use.js
@@ -0,0 +1,8 @@
+export function initVanillaTilt() {
+    VanillaTilt.init(document.querySelectorAll(".card"), {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 1,
+    });
+  }
